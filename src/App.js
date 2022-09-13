b0905d7268e2b0cadaf926801436154408f35f42
FrackingFrick <heta.myllymaki@gmail.com>
2022-09-13 21:21:03 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+  const [age, setAge]=useState(0)
+  const [min, setMin]=useState(0)
+  const [max, setMax]=useState(0)
+
+  function calculate(){
+    setMin((220-age)*0.65)
+    setMax((220-age)*0.85)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Check your heart limits</h3>
+      <form>
+        <div>
+          <label>Age</label>
+          <input type="number" value={age} onChange={e=>setAge(e.target.value)} />
+        </div>
+        <div>
+          <label>Heart rate limits</label>
+          <output>{min.toFixed(0)} - {max.toFixed(0)}</output>
+        </div>
+        <button type="button" onClick={calculate}>Calculate</button> 
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
